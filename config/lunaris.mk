b66eb2b37af1b2881ee8dcc46a4044a65ea2bd9e
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-16 16:11:25 +0000

    vendor: Update add blur support
    Change-Id: I1ced2b3c13c723886aaf46764ba294c18d6ff376
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- config/lunaris.mk
+++ config/lunaris.mk
@@ -25,6 +25,20 @@ PRODUCT_PACKAGES += \
     Updater
 endif
 
+# Enable blur
+TARGET_ENABLE_BLUR ?= true
+ifeq ($(TARGET_ENABLE_BLUR),true)
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=true \
+    persist.sysui.disableBlur=false \
+    ro.surface_flinger.supports_background_blur=1
+else
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=false \
+    persist.sysui.disableBlur=true \
+    ro.surface_flinger.supports_background_blur=0
+endif
+
 # BtHelper
 PRODUCT_PACKAGES += \
     BtHelper
