8d295dc6754dcb9bc0efd1b0ff7993f9d1488e1c
minaripenguin <minaripenguin@users.noreply.github.com>
2025-09-20 09:04:36 +0000

    config: Load boot image profile from frameworks/base
    [YAAP:] art boot image profile is only intended for thin art builds
    without frameworks/base.
    
    Change-Id: I60f3117c6ba948311af9f3fe048d0cf304fc283a
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- config/lunaris.mk
+++ config/lunaris.mk
@@ -41,6 +41,10 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-lunaris-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-lunaris-product.xml
 
+# Use a generic profile based boot image by default
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
+
 # Enable dex2oat64 to do dexopt
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     dalvik.vm.dex2oat64.enabled=true
