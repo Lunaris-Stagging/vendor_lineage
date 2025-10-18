c9e6dcf874e4c518657e681e0e1605624aa2af62
Author Name <quic_prernak@quicinc.com>
2025-10-18 13:09:21 +0000

    config: Set remote provisioning hostname property for android 14
    The remote_provisioning.hostname must be set to remoteprovisioning.googleapis.com
    for devices on Android 14 and above.
    
    Change-Id: I4892edfe68445a0c4fa2f7be8ce6001467c54366
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- config/lunaris.mk
+++ config/lunaris.mk
@@ -164,3 +164,7 @@ endif
 # sound
 PRODUCT_PRODUCT_PROPERTIES += \
     audio.safemedia.bypass=1
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    remote_provisioning.enable_rkpd=true \
+    remote_provisioning.hostname=remoteprovisioning.googleapis.com
