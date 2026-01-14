e29151e173366c1aa3414223fdfbd97d6ee2c61b
someone5678 <someone5678@users.noreply.github.com>
2026-01-14 15:01:43 +0900

    config: art: Use boot image profile from art/build/boot
    Change-Id: I594dd71955272c803f3ac29e176dfe80e15860a6
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

---
+++ config/evolution.mk
@@ -0,0 +1,64 @@
+# Evolution X packages
+PRODUCT_PACKAGES += \
+    EvoEgg
+
+PRODUCT_PACKAGES += \
+    Updater
+
+ifeq ($(WITH_GMS),false)
+PRODUCT_PACKAGES += \
+    UpdaterVanillaOverlay
+endif
+
+# BtHelper
+PRODUCT_PACKAGES += \
+    BtHelper
+
+# ColumbusService
+ifneq ($(TARGET_SUPPORTS_QUICK_TAP),false)
+PRODUCT_PACKAGES += \
+    ColumbusService
+endif
+
+# DeviceAsWebcam
+ifeq ($(TARGET_BUILD_DEVICE_AS_WEBCAM), true)
+    PRODUCT_PACKAGES += \
+        DeviceAsWebcam
+
+    PRODUCT_VENDOR_PROPERTIES += \
+        ro.usb.uvc.enabled=true
+endif
+
+# Face Unlock
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-evolution-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-evolution-product.xml
+
+# Use a generic profile based boot image by default
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_COPY_FILES += \
+    art/build/boot/preloaded-classes:$(TARGET_COPY_OUT_SYSTEM)/etc/preloaded-classes
+
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := \
+    art/build/boot/boot-image-profile.txt
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/etc/preloaded-classes
+
+# Private keys
+ifeq ($(EVO_BUILD_TYPE),Official)
+include vendor/evolution-priv/keys/keys.mk
+else
+-include vendor/evolution-priv/keys/keys.mk
+endif
